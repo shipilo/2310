40eb94c6926380ada26eaf219ca1ef6c8b478e40
shipilo <shadowfire-1@mail.ru>
2021-10-23 11:51:32 +0300

    Добавьте файлы проекта.

---
+++ Les_2310/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Les_2310
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
